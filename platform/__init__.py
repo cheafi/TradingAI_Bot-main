816cf14a2d5d7d13ab32f7f8ca885004e331d3ba
cheafi <135965528+cheafi@users.noreply.github.com>
2025-09-06 18:31:42 +0800

    Fix: remove local 'platform' package to restore stdlib platform

--- platform/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Deprecated local package name 'platform'.
-
-This directory shadows Python's stdlib 'platform'. It should be removed.
-Left empty intentionally so stdlib 'platform' can be used via higher
-precedence in sys.path once this folder is deleted.
-"""
-
-__all__: list[str] = []
